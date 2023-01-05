df747fa09efd14fbff6295e958f5c1e4205fb9fa
Bernardo Cruz Rohlfs <90404673+Berohlfs@users.noreply.github.com>
2023-01-05 17:03:34 -0300

    Add picture zoom function

--- script.js
+++ script.js
@@ -1,22 +1,40 @@
+var card = document.getElementById('div-nasa');
+var zoom = document.getElementById('foto-grande');
+zoom.style.display = 'none';
 
 function carregar() {
-    let xhr = new XMLHttpRequest();
 
+    let xhr = new XMLHttpRequest();
 
     xhr.onload = function () {
 
         let data = JSON.parse(this.responseText);
 
-        document.getElementById('div-nasa').innerHTML =
+        card.innerHTML =
             `
-            <img src="${data.url}" alt="imagem do dia NASA">
+            <img src="${data.url}" onclick="toggle()" alt="imagem do dia NASA">
             <p>${data.date}</p>
             <h1>${data.title}</h1>
             <p class="description">${data.explanation}</p>
             <p>Autor: ${data.copyright}</p>
             `;
+
+        zoom.innerHTML =
+            `
+            <p onclick="toggle()">Voltar</p>
+            <img src="${data.url}" alt="imagem do dia NASA">
+            `;
     }
 
     xhr.open('GET', 'https://api.nasa.gov/planetary/apod?api_key=rF7vbgi86OEdn4j7aVureGB8bDzcO5Y7dDhWrQ7E');
     xhr.send();
+}
+
+function toggle(){
+    if(zoom.style.display == 'none'){
+        zoom.style.display = 'flex';
+    }
+    else{
+        zoom.style.display = 'none';
+    }
 }
