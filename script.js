181144cb1ca2427cf9aa6ea24fe0654d6480b27d
Bernardo Cruz Rohlfs <90404673+Berohlfs@users.noreply.github.com>
2023-01-05 16:29:06 -0300

    Atualização

--- script.js
+++ script.js
@@ -7,15 +7,14 @@ function carregar() {
 
         let data = JSON.parse(this.responseText);
 
-        let nasaFOTD =
+        document.getElementById('div-nasa').innerHTML =
             `
             <img src="${data.url}" alt="imagem do dia NASA">
-            <p class="data_">${data.date}</p>
-            <h1 class="tit_">${data.title}</h1>
-            <p class="expla">${data.explanation}</p>
-            <p class="cr_">Autor: ${data.copyright}</p>
+            <p>${data.date}</p>
+            <h1>${data.title}</h1>
+            <p class="description">${data.explanation}</p>
+            <p>Autor: ${data.copyright}</p>
             `;
-        document.getElementById('nasa').innerHTML = nasaFOTD;
     }
 
     xhr.open('GET', 'https://api.nasa.gov/planetary/apod?api_key=rF7vbgi86OEdn4j7aVureGB8bDzcO5Y7dDhWrQ7E');
