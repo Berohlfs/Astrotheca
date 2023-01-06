92417940970543de6882977f0e105fe78b93c254
Bernardo Cruz Rohlfs <90404673+Berohlfs@users.noreply.github.com>
2023-01-06 12:04:36 -0300

    Revert "remove onload"
    This reverts commit b17dde0a143d98d482ebda2caba4a2b8a56e2929.

--- script.js
+++ script.js
@@ -1,31 +1,34 @@
 var card = document.getElementById('div-nasa');
 var zoom = document.getElementById('foto-grande');
 zoom.style.display = 'none';
-var xhr = new XMLHttpRequest();
 
-xhr.onload = function () {
+function carregar() {
 
-    let data = JSON.parse(this.responseText);
+    let xhr = new XMLHttpRequest();
 
-    card.innerHTML =
-        `
-        <img src="${data.url}" onclick="toggle()" alt="imagem do dia NASA">
-        <p>${data.date}</p>
-        <h2>${data.title}</h1>
-        <p class="description">${data.explanation}</p>
-        <p>Autor: ${data.copyright}</p>
-        `;
+    xhr.onload = function () {
 
-    zoom.innerHTML =
-        `
-        <p onclick="toggle()">Voltar</p>
-        <img src="${data.url}" alt="imagem do dia NASA">
-        `;
+        let data = JSON.parse(this.responseText);
+
+        card.innerHTML =
+            `
+            <img src="${data.url}" onclick="toggle()" alt="imagem do dia NASA">
+            <p>${data.date}</p>
+            <h2>${data.title}</h1>
+            <p class="description">${data.explanation}</p>
+            <p>Autor: ${data.copyright}</p>
+            `;
+
+        zoom.innerHTML =
+            `
+            <p onclick="toggle()">Voltar</p>
+            <img src="${data.url}" alt="imagem do dia NASA">
+            `;
+    }
+
+    xhr.open('GET', 'https://api.nasa.gov/planetary/apod?api_key=rF7vbgi86OEdn4j7aVureGB8bDzcO5Y7dDhWrQ7E');
+    xhr.send();
 }
-
-xhr.open('GET', 'https://api.nasa.gov/planetary/apod?api_key=rF7vbgi86OEdn4j7aVureGB8bDzcO5Y7dDhWrQ7E');
-xhr.send();
-
 
 function toggle(){
     if(zoom.style.display == 'none'){
